380d7fe084429de047a605b2be55e3e6d9b87088
hibana2077 <hibana2077@gmail.com>
2023-06-10 10:46:49 +0800

    update

--- src/train_model.py
+++
@@ -1,8 +0,0 @@
-'''
-Author: hibana2077 hibana2077@gmail.com
-Date: 2023-06-10 10:25:35
-LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2023-06-10 10:25:56
-FilePath: \smart_hydroponic_farm\src\train_model.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
