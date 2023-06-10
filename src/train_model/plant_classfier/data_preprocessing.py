4f86d5841a5bb6494fc74ab4f344c7415c354652
hibana2077 <hibana2077@gmail.com>
2023-06-10 18:21:54 +0800

    add ml python file

---
+++ src/train_model/plant_classfier/data_preprocessing.py
@@ -0,0 +1,11 @@
+'''
+Author: hibana2077 hibana2077@gmail.com
+Date: 2023-06-10 18:20:25
+LastEditors: hibana2077 hibana2077@gmail.com
+LastEditTime: 2023-06-10 18:20:43
+FilePath: \smart_hydroponic_farm\src\train_model\plant_classfier\data_preprocessing.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import pandas as pd
+import numpy as np
+import cv2 as cv
