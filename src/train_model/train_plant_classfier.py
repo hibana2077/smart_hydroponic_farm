17bc3bccecff722025a7be0e0a5edb1fdccd570e
hibana2077 <hibana2077@gmail.com>
2023-06-10 18:14:45 +0800

    add constants

--- src/train_model/train_plant_classfier.py
+++ src/train_model/train_plant_classfier.py
@@ -1,2 +1,18 @@
 import tensorflow as tf
 import numpy as np
+import cv2 as cv
+import os
+import sys
+
+#change path to the directory of this file
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+#------------------Constants------------------
+DATA_DIR_PATH = "../../data/PlantVillage"
+X_TRAIN_PATH = DATA_DIR_PATH + "/X_train.npy"
+Y_TRAIN_PATH = DATA_DIR_PATH + "/Y_train.npy"
+X_TEST_PATH = DATA_DIR_PATH + "/X_test.npy"
+Y_TEST_PATH = DATA_DIR_PATH + "/Y_test.npy"
+MODEL_PATH = "../../models/plant_classifier.h5"
+#---------------------------------------------
+
