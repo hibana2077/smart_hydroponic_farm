6d121bdf2110486c33251f6c39ab45fe7e45d814
hibana2077 <hibana2077@gmail.com>
2023-12-14 19:05:06 +0800

    Update LastEditTime in api.py

--- src/Cloud_compute/api.py
+++ src/Cloud_compute/api.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2023-03-10 13:49:11
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2023-12-14 10:15:33
+LastEditTime: 2023-12-14 10:18:19
 FilePath: \基於滴灌式水耕農法之改進研究以及實作\Cloud_compute\api.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
