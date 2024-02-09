be4cc07c02414f573fb80ebc0160b74c09cf426b
hibana2077 <hibana2077@gmaill.com>
2024-02-09 18:00:57 +0800

    Delete unnecessary files and update .gitignore

---
+++ src/train_model/diseas_predict/train.py
@@ -0,0 +1,11 @@
+'''
+Author: hibana2077 hibana2077@gmaill.com
+Date: 2024-02-09 11:48:59
+LastEditors: hibana2077 hibana2077@gmaill.com
+LastEditTime: 2024-02-09 13:28:20
+FilePath: /smart_hydroponic_farm/src/train_model/diseas_predict/train.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import os
+os.environ['KERAS_BACKEND'] = 'torch'
+import keras
